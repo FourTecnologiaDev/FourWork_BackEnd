c577c10aba942539d9aa031951155e14c8070b14
Pedro TortoriellO <pedrooofreitas@gmail.com>
2024-02-28 10:44:01 -0300

    28/02

--- src/routes/ticket.js
+++ src/routes/ticket.js
@@ -8,14 +8,16 @@ router.post('/ticket', async (req, res) => {
   try {
     const newTicket = new Ticket(req.body); // Crie um novo ticket usando os dados recebidos no corpo da requisição
     await newTicket.save(); // Salve o novo ticket no banco de dados
-    res.status(201).json(newTicket); // Responda com o ticket criado e um status 201 (Created)
 
-    retorno = await crud("tabela", req.body, "ticket");
-    res.send(retorno).end();
+    // Executar a função crud
+    const retorno = await crud("tabela", req.body, "ticket");
+
+    res.status(201).json(newTicket); // Responda com o ticket criado e um status 201 (Created)
   } catch (error) {
     console.error('Erro ao criar ticket:', error);
     res.status(500).json({ message: 'Erro interno do servidor' }); // Responda com um status 500 (Internal Server Error) se ocorrer um erro
   }
 });
+
 
 module.exports = router;
