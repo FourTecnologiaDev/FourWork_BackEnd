b646c27e8c7524dbaa1ec04053bf4f436e02b680
Pedro TortoriellO <pedrooofreitas@gmail.com>
2024-02-28 12:49:30 -0300

    28/02

--- src/schema/login.js
+++ src/schema/login.js
@@ -11,7 +11,8 @@ function mongoSchemaLogin() {
     },
     email: {
       type: String,
-      require: true
+      require: true,
+      unique: true
     }, 
 	  password: {
       type: String,
