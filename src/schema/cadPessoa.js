b6e50ba0b0799ffc5f4ed679416ce8b46ee33fe8
Pedro TortoriellO <pedrooofreitas@gmail.com>
2024-03-25 20:48:47 -0400

    25/03

--- src/schema/cadPessoa.js
+++ src/schema/cadPessoa.js
@@ -1,60 +1,66 @@
 const mongoose = require("mongoose");
 
+let CadastroPessoa;
+
+// Verifica se o modelo já foi definido antes de tentar defini-lo novamente
 function mongoSchemaCadastroPessoa() {
-  const ticketSchema = new mongoose.Schema({
-    codigo: {
-      type: Number,
-      required: true
-    },
-    fornecedor: {
-      type: Number,
-      required: true
-    },
-    CNPJ: {
-      type: Number,
-    }, 
-    CPF: {
-      type: Number,
-    }, 
-    Nome: {
-      type: String,
-    }, 
-    Email: {
-      type: String,
-    }, 
-    TipoPessoa: {
-      type: String,
-    }, 
-    CEP: {
-      type: Number,
-    }, 
-    Endereço: {
-      type: String,
-    }, 
-    Numero: {
-      type: Number,
-    },
-    Telefone: {
-      type: String,
-    },
-    ValorH: {
-      type: Number,
-    },
-    HorasT: {
-      type: Number,
-    },
-    ValorAdc: {
-      type: Number,
-    },
-    Data: {
-      type: String,
-    },
-  });
+  if (!CadastroPessoa) {
+    const cadastroSchema = new mongoose.Schema({
+      codigo: {
+        type: Number,
+      },
+      fornecedor: {
+        type: Number,
+      },
+      CNPJ: {
+        type: Number,
+      }, 
+      CPF: {
+        type: Number,
+      }, 
+      nomePessoa: {
+        type: String,
+      }, 
+      Email: {
+        type: String,
+      }, 
+      tipoPessoa: {
+        type: String,
+      }, 
+      CEP: {
+        type: String,
+      }, 
+      Endereço: {
+        type: String,
+      }, 
+      Número: {
+        type: Number,
+      },
+      Telefone: {
+        type: String,
+      },
+      ValorH: {
+        type: Number,
+      },
+      HorasT: {
+        type: Number,
+      },
+      ValorAdc: {
+        type: Number,
+      },
+      Data: {
+        type: String,
+      },
+      diasUteis: {
+        type: String,
+      },
+    });
 
+    // Define the model with the correct name
+    CadastroPessoa = mongoose.model('cadPessoa', cadastroSchema);
+  }
 
-  
-  // Modelo para a coleção "tickets"
-  return mongoose.model('cadastroPessoa', ticketSchema);
+  return CadastroPessoa;
 }
 
 module.exports = mongoSchemaCadastroPessoa;
