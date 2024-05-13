7949263e5c770dd0689f8c7f402e2264c905aade
Pedro TortoriellO <pedrooofreitas@gmail.com>
2024-05-13 11:47:48 -0400

    13/05

--- src/crud.js
+++ src/crud.js
@@ -20,28 +20,6 @@ async function crud(tabela, registro, operacao) {
         var codigo = 1;
     }      
 
-    // Mascarar dados
-    if(operacao == 'mascarar'){
-        registro.crm = '**********';
-        registro.cpf = '**********';
-        registro.cep = '**********';
-        registro.endereco = '**********';
-        registro.numero = '**********';
-        registro.endereco2 = '**********';
-        registro.cidade = '**********';
-        registro.estado = '**********';
-        registro.email = '**********';
-        registro.tel = '**********';
-        registro.whats = '**********';
-        registro.nascimento = '**********';
-        registro.identidade = '**********';
-        registro.passporte = '**********';
-        registro.complemento = '**********';
-        
-        json = JSON.stringify({"codigoEvento": registro.codigoEvento});
-        filter = JSON.parse(json);
-        modelo.updateMany(filter, registro).exec();  
-    }
 
     // Busca registro
     if(operacao == 'find'){
@@ -64,38 +42,6 @@ async function crud(tabela, registro, operacao) {
         //await crud(tabela, '','lastCode')
         new modelo(registro).save();
     }
-
-    // Insere multiplos registro
-    if(operacao == 'insertMany'){
-        resultado = await modelo.insertMany(registro);
-    }
-
-    // Atualiza registro
-    if(operacao == 'update'){        
-        id = registro._id;
-        json = JSON.stringify({"_id": id});
-        var filter = JSON.parse(json);
-        delete registro.codigo;
-        
-        if(registro.historico){
-            resultado = modelo.findByIdAndUpdate(id,{$addToSet: { historico: registro.historico[0] } }).exec();
-            delete registro.historico;           
-            modelo.findOneAndUpdate(filter, registro).exec();
-        }
-        else {
-            delete registro._id
-            resultado = modelo.findOneAndUpdate(filter, registro).exec();  
-        }
-    }
-        // Atualiza registro arrays participantes
-        if(operacao == 'updateArrays'){        
-            let id = registro._id;
-            if(registro._id)
-                delete registro._id
-            let array = Object.keys(registro)
-            //resultado = modelo.findByIdAndUpdate(id,{$addToSet: { aereo: registro.aereo } }).exec();                
-            resultado = modelo.findByIdAndUpdate(id,{ $set: { aereo: registro.aereo } },  { new: false }).exec();              
-        }
     
     // Busca ultimo registro
     if(operacao == 'lastCode'){
