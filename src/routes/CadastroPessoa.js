c3e7b8141f508d8e0e45e2d52f796d373c03b135
Pedro TortoriellO <pedrooofreitas@gmail.com>
2024-05-13 12:05:13 -0400

    13/05

--- src/routes/CadastroPessoa.js
+++ src/routes/CadastroPessoa.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const cors = require('cors');
 const crud = require("../crud");
-const cors = require('cors');
 
 router.use(cors());
 
